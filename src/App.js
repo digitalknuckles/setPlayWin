93d9abe346fe3aa7bc225214a65df767ca212ac3
digitalknuckles <digitalknuckles@gmail.com>
2024-11-23 11:13:04 -0500

    initial update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+const content = [
+  ["something goes here", "something goes here", "something goes here", "something goes here"],
+  ["something goes here", "something goes here", "something goes here", "something goes here"],
+  ["something goes here", "something goes here", "something goes here", "something goes here"],
+  ["something goes here", "something goes here", "something goes here", "something goes here"]
+];
+
+export default function App() {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src="react-logo-xs.png" alt="React logo" />
+        <div>
+          <h1>SetPlayWin</h1>
+          <p>i.e., using the React library for rendering the UI</p>
+        </div>
       </header>
+      <div id="tabs">
+        <menu>
+          <button
+            className={activeContentIndex === 0 ? "active" : ""}
+            onClick={() => setActiveContentIndex(0)}
+          >
+            Why React?
+          </button>
+          <button
+            className={activeContentIndex === 1 ? "active" : ""}
+            onClick={() => setActiveContentIndex(1)}
+          >
+            Core Features
+          </button>
+          <button
+            className={activeContentIndex === 2 ? "active" : ""}
+            onClick={() => setActiveContentIndex(2)}
+          >
+            Related Sources
+          </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            Contact
+          </button>
+        </menu>
+        <div id="tab-content">
+          <ul>
+            {content[activeContentIndex].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
